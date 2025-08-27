e32bcf62407787e339396ef3cd82f7f804882a53
herdialfachri <alfachri75@gmail.com>
2025-08-27 20:31:27 +0700

    Modify user model to have relation with services and ratings model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,25 +10,24 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'role_id', // gunakan nama kolom sebenarnya
+        'role_id', // kolom role
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -36,7 +35,7 @@ class User extends Authenticatable implements MustVerifyEmail
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
      * @return array<string, string>
      */
@@ -46,5 +45,21 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Relasi ke jasa (services) yang dimiliki user/tukang
+     */
+    public function services()
+    {
+        return $this->hasMany(Service::class, 'user_id', 'id');
+    }
+
+    /**
+     * Relasi ke rating yang diberikan user
+     */
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class, 'user_id', 'id');
     }
 }
